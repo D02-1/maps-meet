9a165a025c0e8b2c4be95d47d248bf07b2521dc4
Alkhaled <almzrab@gmail.com>
2022-03-28 15:09:20 +0200

    add filse structure

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5000;
+
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile( path.join(__dirname,'../dist' , 'index.html'));
+})
+
+app.listen(port,()=>{
+    console.log( ` server listening on ${port}` );
+});
